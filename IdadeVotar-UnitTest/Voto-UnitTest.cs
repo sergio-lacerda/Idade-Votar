476bc8c8866a7287dee7b4fdff7b9e027643ae66
Sérgio Lacerda <cursos@sergiolacerda.com.br>
2021-11-02 16:41:46 -0300

    Adicionando arquivos do projeto

---
+++ IdadeVotar-UnitTest/Voto-UnitTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IdadeVotar;
+using Xunit;
+
+namespace IdadeVotar_UnitTest
+{
+    public class Voto_UnitTest
+    {
+        [Theory]
+        [InlineData(14, "Não pode votar")]
+        [InlineData(17, "Voto opcional")]
+        [InlineData(21, "Pode votar")]
+        [InlineData(16, "Voto opcional")]
+        [InlineData(18, "Pode votar")]
+        [InlineData(-15, "Valor inválido")]
+        [InlineData(200, "Valor inválido")]
+        public void deveRetornarPodeVotar(int vIdade, string esperado)
+        {
+            //Act
+            var obtido = Voto.podeVotar(vIdade);
+
+            //Assert
+            Assert.Equal(esperado, obtido);
+        }
+    }
+}
---
+++ IdadeVotar/Voto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdadeVotar
+{
+    public static class Voto
+    {
+        public static string podeVotar(int vIdade) 
+        {
+			/*   Código inicial - Antes de executar os testes 
+			String voto = "";
+			if (vIdade < 16)
+				voto = "Não pode votar";
+			else
+			if ((vIdade > 16) && (vIdade < 18))
+				voto = "Voto opcional";
+			else
+				if (vIdade > 18)
+					voto = "Pode votar";
+				return voto;
+			*/
+
+			/* Código refatorado - Após executar os testes */
+			String voto = "";
+			if ( vIdade < 0 || vIdade > 130 )
+				voto = "Valor inválido";
+			else 
+			if (vIdade < 16)
+				voto = "Não pode votar";
+			else
+			if ((vIdade >= 16) && (vIdade < 18))
+				voto = "Voto opcional";
+			else
+				if (vIdade >= 18)
+				voto = "Pode votar";
+			return voto;
+		}
+	}
+}
